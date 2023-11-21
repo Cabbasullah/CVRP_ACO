6be5ecffd9f8829dfd452e3493a18109e4d84fc1
AbbasIman <85749516+Cabbasullah@users.noreply.github.com>
2023-11-22 06:21:04 +0700

    Add files via upload

---
+++ node.py
@@ -0,0 +1,44 @@
+from utility import distance
+
+class Customer:
+    def __init__(self, x, y, demand=0):
+        self.x = x
+        self.y = y
+        self.demand = demand
+
+
+class Vehicle:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.route = []
+        self.visited = set()
+
+
+class Depot:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+
+nodes = [
+    Depot(600, 500),  # Depot
+    Customer(400, 400, demand=10),
+    Customer(500, 400, demand=5),
+    Customer(600, 300, demand=8),
+    Customer(700, 400, demand=3),
+    Customer(600, 400, demand=2),
+    Customer(500, 500, demand=6),
+    Customer(600, 600, demand=7),
+    Customer(700, 500, demand=9),
+]
+
+# Create distance matrix for faster distance calculations
+distance_matrix = [[distance(node1, node2)
+                    for node2 in nodes] for node1 in nodes]
+
+
+# Define additional utility function to calculate total distance for a given route
+def calculate_total_distance(route):
+    return sum(distance(route[i], route[i + 1]) for i in range(len(route) - 1))
